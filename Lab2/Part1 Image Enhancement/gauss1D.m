8a9310ac7e2b57f1244fc732786beff6168ed39f
apauliuc <andupauliuc@gmail.com>
2018-02-22 18:08:11 +0100

    Implement 1D and 2D Gaussian filters.

--- Lab2/Part1 Image Enhancement/gauss1D.m
+++ Lab2/Part1 Image Enhancement/gauss1D.m
@@ -4,4 +4,11 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    % create initial vector of size kernel_size, centered around 0
+    floored_size = floor(kernel_size / 2);
+    kernel = (-floored_size:floored_size);
+    
+    % compute 1D Gaussian filter by formula and normalize it
+    G = 1 / (sigma * sqrt(2 * pi)) * exp(- kernel .^ 2 / (2 * sigma ^ 2));
+    G = G / sum(G);
 end
--- Lab2/Part1 Image Enhancement/gauss2D.m
+++ Lab2/Part1 Image Enhancement/gauss2D.m
@@ -1,3 +1,7 @@
 function G = gauss2D( sigma , kernel_size )
-    %% solution
+    % get 1D Gaussian filter with given parameters
+    G_1D = gauss1D(sigma, kernel_size);
+    % multiply the transpose of 1D Gaussian filter with itself
+    % gives a (kernel_size x kernel_size) matrix
+    G = transpose(G_1D) * G_1D;
 end
