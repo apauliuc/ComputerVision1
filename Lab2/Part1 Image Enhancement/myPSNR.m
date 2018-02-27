86982bf45edc67b3e2592ab481a7887f21247f12
Renzo <renzovanslooten@msn.com>
2018-02-27 21:31:56 +0100

    myPSNR and createGabor

--- Lab2/Part1 Image Enhancement/myPSNR.m
+++ Lab2/Part1 Image Enhancement/myPSNR.m
@@ -1,5 +1,7 @@
-function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
+% myPSNR(double(imread('images/image1_saltpepper.jpg')),double(imread('images/image1.jpg')))
 
+function [ PSNR ] = myPSNR( orig_image, approx_image )
+error = orig_image - approx_image;
+PSNR = 20*log10(max(orig_image(:))/(sqrt(mean(error(:).^2))));
 end
 
