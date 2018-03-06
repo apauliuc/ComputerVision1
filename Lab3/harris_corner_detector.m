6475ef6d9937aa232524d28a0c987c2fcc4486dc
Renzo <renzovanslooten@msn.com>
2018-03-06 16:55:35 +0100

    Changed code to follow explanation from assignment

--- Lab3/harris_corner_detector.m
+++ Lab3/harris_corner_detector.m
@@ -1,55 +1,44 @@
 close all; clear; clc;
 
 k = 0.04;
-Threshold = 10000000;
-sigma = 1;
-halfwid = sigma * 3;
-
-[xx, yy] = meshgrid(-halfwid:halfwid, -halfwid:halfwid);
-
-Gxy = exp(-(xx .^ 2 + yy .^ 2) / (2 * sigma ^ 2));
-
-Gx = xx .* exp(-(xx .^ 2 + yy .^ 2) / (2 * sigma ^ 2));
-Gy = yy .* exp(-(xx .^ 2 + yy .^ 2) / (2 * sigma ^ 2));
+sigma = 4;
+Threshold = 15000;
 
 I = rgb2gray(imread('person_toy/00000001.jpg'));
+
+half_wid = (sigma * 3 - 1)/2;
+[dx, dy] = meshgrid(-half_wid:half_wid, -half_wid:half_wid);
+
+Gxy = fspecial('gaussian', half_wid*2+1, sigma);
+Gx = (dx .* Gxy);
+Gy = (dy .* Gxy);
+
+% 1) Compute x and y derivatives of image
+Ix = conv2(I, Gx, 'same');
+Iy = conv2(I, Gy, 'same');
+
+% 2) Compute the sums of the products of derivatives at each pixel
+Ix2 = conv2(Ix .^ 2, Gxy, 'same');
+Iy2 = conv2(Iy .^ 2, Gxy, 'same');
+Ixy = conv2(Ix .* Iy, Gxy, 'same');
+
+% 3) Define at each pixel the matrix H
+H = (Ix2.*Iy2 - Ixy.^2) - k*(Ix2 + Iy2).^2;
 
 numOfRows = size(I, 1);
 numOfColumns = size(I, 2);
 
-% 1) Compute x and y derivatives of image
-Ix = conv2(Gx, I);
-Iy = conv2(Gy, I);
-
-size(Ix)
-
-% 2) Compute products of derivatives at every pixel
-Ix2 = Ix .^ 2;
-Iy2 = Iy .^ 2;
-Ixy = Ix .* Iy;
-
-% 3)Compute the sums of the products of derivatives at each pixel
-Sx2 = conv2(Gxy, Ix2);
-Sy2 = conv2(Gxy, Iy2);
-Sxy = conv2(Gxy, Ixy);
-
 im = zeros(numOfRows, numOfColumns);
 for x=1:numOfRows
    for y=1:numOfColumns
-       % 4) Define at each pixel(x, y) the matrix H
-       Q = [Sx2(x, y) Sxy(x, y); Sxy(x, y) Sy2(x, y)];
-       
-       % 5) Compute the response of the detector at each pixel
-       H = det(Q) - k * (trace(Q) ^ 2);
-       
-       % 6) Threshold on value of R
-       if (H > Threshold)
-          im(x, y) = H; 
+       % 4) Threshold on value of R
+       if (H(x,y) > Threshold)
+          im(x, y) = H(x,y); 
        end
    end
 end
 
-% 7) Compute nonmax suppression
+% 5) Compute nonmax suppression
 output = im > imdilate(im, [1 1 1; 1 0 1; 1 1 1]);
 
 figure, imshow(I);
