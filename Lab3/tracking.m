baa9204343915a94f3d2d7a98a43965ab088218f
apauliuc <andupauliuc@gmail.com>
2018-03-09 19:41:27 +0100

    Prepare tracking algo for submission

--- Lab3/tracking.m
+++ Lab3/tracking.m
@@ -1,27 +1,24 @@
+function tracking (image_set, save_to_gif, file_extension)
 % Using Harris Corner Detector, locate feature points and track them in
-% a given sequence of images
-close all; clear; clc;
-image_set = 'person_toy';
-save_to_gif = false;
+% a given sequence of images with Lucas-Kanade algorithm
 save_file = strcat(image_set, '.gif');
 h = figure;
 
 if strcmp(image_set, 'pingpong')
     images_folder = 'pingpong';
-    filePattern = fullfile(images_folder, '*.jpeg');
+    filePattern = fullfile(images_folder, file_extension);
 else
     images_folder = 'person_toy';
-    filePattern = fullfile(images_folder, '*.jpg');
+    filePattern = fullfile(images_folder, file_extension);
 end
 
 % read all files in directory that match the pattern
 x = zeros(1); y = zeros(1);
 images = dir(filePattern);
 for k = 1 : length(images)
-    baseFileName = images(k).name;
-    fullFileName = fullfile(images_folder, baseFileName);
-%     fprintf(1, 'Now reading %s\n', fullFileName);
-    curr_img = rgb2gray(imread(fullFileName));
+    base_file_name = images(k).name;
+    full_file_name = fullfile(images_folder, base_file_name);
+    curr_img = rgb2gray(imread(full_file_name));
     
     if k == 1
         % for the first image, locate feature points
@@ -61,4 +58,80 @@ for k = 1 : length(images)
             imwrite(imind, cm, save_file, 'gif', 'WriteMode', 'append', 'DelayTime',0.2);
         end
     end
+end
+end
+
+function [x, y] = tracking_locate_points(I)
+%TRACKING_LOCATE_POINTS Locate feature points from input image
+%   Uses Harris Corner Detector algorithm to localise feature points
+sigma = 3;5;
+thres = 16000;
+window_N = 14;
+
+half_wid = (sigma * 3 - 1)/2;
+[dx, dy] = meshgrid(-half_wid:half_wid, -half_wid:half_wid);
+Gxy = fspecial('gaussian', half_wid*2+1, sigma);
+Gx = (dx .* Gxy);
+Gy = (dy .* Gxy);
+
+Ix = conv2(I, Gx, 'same');
+Iy = conv2(I, Gy, 'same');
+
+Ix2 = conv2(Ix .^ 2, Gxy, 'same');
+Iy2 = conv2(Iy .^ 2, Gxy, 'same');
+Ixy = conv2(Ix .* Iy, Gxy, 'same');
+
+myHarris = (Ix2.*Iy2 - Ixy.^2) - 0.04*(Ix2 + Iy2).^2;
+
+thresholded_harris = myHarris;
+thresholded_harris(myHarris<thres) = 0;
+
+window = true(window_N);
+window(ceil(window_N/2),ceil(window_N/2)) = 0;
+output = thresholded_harris > imdilate(thresholded_harris, window);
+[y, x] = find(output == 1);
+end
+
+function [V, points_x, points_y] = tracking_optical_flow(img1, img2, points_x, points_y, window_size)
+%TRACKING_OPTICAL_FLOW Apply Lucas-Kanade method to track the interest
+%points with coordinates (x,y)
+half = round(window_size/2);
+pad = half - 1;
+
+% compute partial derivatives in x, y, and t directions
+Ix_full = conv2(img1, [-1 1; -1 1], 'same');
+Iy_full = conv2(img1, [-1 -1; 1 1], 'same');
+It_full = conv2(img1, ones(2), 'same') + conv2(img2, -ones(2), 'same');
+
+V = zeros(size(points_x, 1), 2);
+
+% for each region, take the Ix, Iy and It, compute A, At, b and Vx and Vy
+for idx = 1:size(points_x, 1)
+    x = round(points_x(idx));
+    y = round(points_y(idx));
+    
+    lower_x = max(x-pad, 1);
+    upper_x = min(x+pad, size(img1, 1));
+    
+    lower_y = max(y-pad, 1);
+    upper_y = min(y+pad, size(img1, 2));
+    
+    % slice matrices to get values for pixels in region
+    Ix = Ix_full(lower_x:upper_x, lower_y:upper_y);
+    Iy = Iy_full(lower_x:upper_x, lower_y:upper_y);
+    It = It_full(lower_x:upper_x, lower_y:upper_y);
+
+    % compute A, At, b
+    Ix = Ix(:);
+    Iy = Iy(:);
+    b = -It(:);
+    A = [Ix Iy];
+
+    % compute V = (Vx, Vy) and place in matrix
+    v_at_point = pinv(A) * b;
+
+    V(idx, :) = reshape(v_at_point, 1, 2);
+    points_x(idx) = points_x(idx) + v_at_point(2);
+    points_y(idx) = points_y(idx) + v_at_point(1);
+end
 end
---
+++ Lab3/tracking_demo.m
@@ -0,0 +1,11 @@
+%% Feature tracking on pingpong example
+close all; clear; clc;
+
+% for saving to gif, set 2nd parameter to true
+tracking('pingpong', false, '*.jpeg'); 
+
+%% Feature tracking on person_toy example
+close all; clear; clc;
+
+% for saving to gif, set 2nd parameter to true
+tracking('person_toy', false, '*.jpg');
